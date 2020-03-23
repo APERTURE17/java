99251727c3165c1115f6c93ced26922f5ca0bcee
APERTURE17 <1538902396@qq.com>
2020-03-23 21:51:48 +0800

    20200323

---
+++ idea20200323/src/TestDemo.java
@@ -0,0 +1,22 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addLast(3);
+        myLinkedList.addLast(4);
+        myLinkedList.addFirst(5);
+        myLinkedList.addFirst(6);
+        myLinkedList.addIndex(2,7);
+        myLinkedList.display();
+        System.out.println(myLinkedList.contains(4));
+        System.out.println(myLinkedList.contains(17));
+        System.out.println(myLinkedList.size());
+        myLinkedList.remove(4);
+        myLinkedList.display();
+        myLinkedList.addFirst(2);
+        myLinkedList.addFirst(2);
+        myLinkedList.addFirst(2);
+        myLinkedList.display();
+        myLinkedList.removeAllKey(2);
+        myLinkedList.display();
+    }
+}
